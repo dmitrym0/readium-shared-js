c15345bd51201e06c83dde1d7655c8529c775a09
Dmitry M <dmitrym@evidentpoint.com>
2015-07-03 12:28:17 -0700

    Fix iframe names based on Jonahs feedback. iFrame names are now unique

--- js/helpers.js
+++ js/helpers.js
@@ -401,23 +401,25 @@ ReadiumSDK.Helpers.Margins.empty = function() {
  * @returns {ReadiumSDK.Helpers.loadTemplate.cache}
  */
 ReadiumSDK.Helpers.loadTemplate = function(name, params) {
-    return ReadiumSDK.Helpers.loadTemplate.cache[name];
+    var uniqueIframeId = _.uniqueId("-uid-");
+   /**
+     *
+     * @type {{fixed_book_frame: string, single_page_frame: string, scrolled_book_frame: string, reflowable_book_frame: string, reflowable_book_page_frame: string}}
+     */
+    var template =  {
+        "fixed_book_frame" : '<div id="fixed-book-frame' + uniqueIframeId + '" class="clearfix book-frame fixed-book-frame"></div>',
+        
+        "single_page_frame" : '<div><div id="scaler"><iframe scrolling="no" class="iframe-fixed"></iframe></div></div>',
+        //"single_page_frame" : '<div><iframe scrolling="no" class="iframe-fixed" id="scaler"></iframe></div>',
+        
+        "scrolled_book_frame" : '<div id="reflowable-book-frame' + uniqueIframeId + '" class="clearfix book-frame reflowable-book-frame"><div id="scrolled-content-frame"></div></div>',
+        "reflowable_book_frame" : '<div id="reflowable-book-frame' + uniqueIframeId + '" class="clearfix book-frame reflowable-book-frame"></div>',
+        "reflowable_book_page_frame": '<div id="reflowable-content-frame" class="reflowable-content-frame"><iframe scrolling="no" id="epubContentIframe"></iframe></div>'
+    };
+    return template[name];
 };
 
-/**
- *
- * @type {{fixed_book_frame: string, single_page_frame: string, scrolled_book_frame: string, reflowable_book_frame: string, reflowable_book_page_frame: string}}
- */
-ReadiumSDK.Helpers.loadTemplate.cache = {
-    "fixed_book_frame" : '<div id="fixed-book-frame" class="clearfix book-frame fixed-book-frame"></div>',
-    
-    "single_page_frame" : '<div><div id="scaler"><iframe scrolling="no" class="iframe-fixed"></iframe></div></div>',
-    //"single_page_frame" : '<div><iframe scrolling="no" class="iframe-fixed" id="scaler"></iframe></div>',
-    
-    "scrolled_book_frame" : '<div id="reflowable-book-frame" class="clearfix book-frame reflowable-book-frame"><div id="scrolled-content-frame"></div></div>',
-    "reflowable_book_frame" : '<div id="reflowable-book-frame" class="clearfix book-frame reflowable-book-frame"></div>',
-    "reflowable_book_page_frame": '<div id="reflowable-content-frame" class="reflowable-content-frame"><iframe scrolling="no" id="epubContentIframe"></iframe></div>'
-};
+
 
 /**
  *
